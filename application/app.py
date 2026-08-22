2c1fc7d74de6d02f9bc8bc81b1d80e57630f7b12
omprakash <omejjigiri99@gmail.com>
2026-08-22 15:15:22 +0530

    first project

--- application/app.py
+++ application/app.py
@@ -2,4 +2,6 @@ def add(a,b):
     return a + b
 result = add(10,20)
 
-print(result)
+if __name__ == "__main__":
+    print("application started")
+    print(f"result: ", result)
---
+++ application/test_app.py
@@ -0,0 +1,5 @@
+from app import add
+
+def test_add():
+    assert add(15,30) == 45
+    
